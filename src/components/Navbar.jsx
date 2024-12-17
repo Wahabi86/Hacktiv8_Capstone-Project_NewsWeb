ae0586aa98d1d16a85bd2470e96c8f85cad95983
Wahabi86 <gustiabi97@gmail.com>
2024-12-17 13:48:33 +0800

    update navbar icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,26 +18,31 @@ function Navbar() {
     <>
       <nav className="navbar sticky-top navbar-expand-lg navbar-dark p-0">
         <div className="container">
-          <Link className="navbar-brand text-white px-3 d-flex align-items-center" style={{ height: "55px" }} to="/">
-            Home
+          <Link className="navbar-brand text-white px-3 mt-2 d-flex align-items-center" to="/">
+            <h3><span>N</span>EWS</h3>
           </Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0 d-flex flex-row align-items-center">
               <li className="nav-item">
-                <Link className="nav-link text-white" to="/indonesia">
+                <Link className="nav-link text-white px-3 d-flex align-items-center" to="/" style={{ backgroundColor: "#c62e2e", height: "60px" }}>
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link text-white px-2 d-flex align-items-center" to="/indonesia">
                   Indonesia
                 </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link text-white" to="/programming">
+                <Link className="nav-link text-white px-2 d-flex align-items-center" to="/programming">
                   Programming
                 </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link text-white" to="/saved">
+                <Link className="nav-link text-white px-2 d-flex align-items-center" to="/saved">
                   Saved
                 </Link>
               </li>
